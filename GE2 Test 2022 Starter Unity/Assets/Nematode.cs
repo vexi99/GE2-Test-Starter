fe8cb8e8687d197389cf3f778a1e904e34e877d9
David <davidrtilson@gmail.com>
2022-04-26 09:41:54 +0100

    work on nematode spwan

--- GE2 Test 2022 Starter Unity/Assets/Nematode.cs
+++ GE2 Test 2022 Starter Unity/Assets/Nematode.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Nematode : MonoBehaviour
 {
-    public int length = 5;
+    public int length; //To Be Set Random
 
     public Material material;
+    public GameObject head;
 
     void Awake()
     {
@@ -17,12 +18,20 @@ public class Nematode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        length = Random.Range(0,20); //random length between 0 and 20
+        CreateNematode();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        head.transform.position += new Vector3(0, 10, 0);
+    }
+
+    void CreateNematode()
+    {
+        Vector3 test = new Vector3(0, 0, 0);
+        Instantiate(head, test,Quaternion.identity);
+        head.transform.position = new Vector3(0, 10, 0);
     }
 }
