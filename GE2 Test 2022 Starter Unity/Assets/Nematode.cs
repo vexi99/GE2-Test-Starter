7a788ff07a04c8add43fc5d7d9297dbe0189438e
David <davidrtilson@gmail.com>
2022-04-26 10:54:34 +0100

    width work finished, unable to complete parent

--- GE2 Test 2022 Starter Unity/Assets/Nematode.cs
+++ GE2 Test 2022 Starter Unity/Assets/Nematode.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class Nematode : MonoBehaviour
 {
     public int length; //To Be Set Random in Awake() function
+    public int startXPos, startYPos; //Starting X and Y Coords
 
     public Material material;
     public GameObject head;
+    public GameObject[] body;
 
     void Awake()
     {
         // Put your code here!
         length = Random.Range(3,15);
+
+        body = new GameObject[length];
 
         CreateNematode();
     }
@@ -30,28 +34,40 @@ public class Nematode : MonoBehaviour
         
     }
 
+    void NematodeMovement()
+    {
+        
+    }
+
     void CreateNematode()
     {
+        //Create Random starting Position
+        startXPos = 0; //Random.Range(-100,100);
+        startYPos = 0; //Random.Range(-100,100);
+
+        
+
         //For loop of random length deciding how many spheres are created for Nematode Body Length
         for(int i = 0; i < length; i++)
         {
-            GameObject body = GameObject.Instantiate<GameObject>(head);
 
-            body.transform.position = transform.TransformPoint(new Vector3(0,0,i)); //spawn 1 unit away from last
-            body.transform.parent = body.transform; //trying to set body component as a child
+            body[i] = GameObject.Instantiate<GameObject>(head);
+
+            body[i].transform.position = transform.TransformPoint(new Vector3(startXPos,startYPos,i)); //spawn 1 unit away from last
+            //body[i].transform.parent = transform; //trying to set body component as a child
 
             //changing with of nematode as it lengthens
             if(i < length/2)
             {
                 //making skinnier
                 float width = (1.0f / (length / 2) * i);
-                body.transform.localScale = new Vector3(width, width, body.transform.localScale.z);
+                body[i].transform.localScale = new Vector3(width, width, body[i].transform.localScale.z);
             }
             else
             {
                 //making wider
                 float width = (1.0f * (length / 2) / i);
-                body.transform.localScale = new Vector3(width, width, body.transform.localScale.z);
+                body[i].transform.localScale = new Vector3(width, width, body[i].transform.localScale.z);
             }
             
         }   
